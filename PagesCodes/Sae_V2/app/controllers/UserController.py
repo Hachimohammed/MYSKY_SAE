8b2ed9c8fa77b21be839c07db36f45e08593ec56
Yunes <yunes.bouabta@gmail.com>
2026-01-12 18:27:41 +0100

    Mise en commentaire de la partie admin qui bug pour coder de mon coteé

--- PagesCodes/Sae_V2/app/controllers/UserController.py
+++ PagesCodes/Sae_V2/app/controllers/UserController.py
@@ -3,31 +3,31 @@ from app import app
 from app.models.UserDAO import UserSqliteDAO as UserDAO
 from app.models.BDDao import DatabaseInit as BDDao
 
-@app.route('/add_user', methods=['POST'])
-def addUser():
-    nom = request.form['firstname']
-    prenom = request.form['name']
-    email = request.form['email']
-    mdp = BDDao._generatePwdHash(request.form['password'])
-    id_Groupe = int(request.form['role'])
+#@app.route('/add_user', methods=['POST'])
+#def addUser():
+#    nom = request.form['firstname']
+#    prenom = request.form['name']
+#    email = request.form['email']
+#    mdp = BDDao._generatePwdHash(request.form['password'])
+#    id_Groupe = int(request.form['role'])
+#
+#    UserDAO.addUser(prenom, nom, email, mdp, id_Groupe)
+#
+#   return redirect(url_for('admin'))
 
-    UserDAO.addUser(prenom, nom, email, mdp, id_Groupe)
-
-    return redirect(url_for('admin'))
-
-@app.route('/admin')
-def admin_page():
-    groupes = UserDAO.getGroupes()
-    return render_template('admin.html', groupes=groupes)
+#@app.route('/admin')
+#def admin_page():
+#    groupes = UserDAO.getGroupes()
+#    return render_template('admin.html', groupes=groupes)
 
 
-@app.route('/delete_user/<int:user_id>', methods=['POST'])
-def deleteUser(user_id):
+#@app.route('/delete_user/<int:user_id>', methods=['POST'])
+#def deleteUser(user_id):
 
-    if user_id == session['user_id']:
-        return redirect(url_for('admin_page'))
+#    if user_id == session['user_id']:
+#        return redirect(url_for('admin_page'))
     #empecher admin de se supprimer lui meme
 
-    UserDAO.deleteUser(user_id)
-    return redirect(url_for('admin'))
+#    UserDAO.deleteUser(user_id)
+#    return redirect(url_for('admin'))
 
