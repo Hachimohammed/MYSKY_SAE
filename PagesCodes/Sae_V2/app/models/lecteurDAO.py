e84f19290b7c4cdb6400175065ccc7a4702dc414
kenshivagabond <naekenshi@gmail.com>
2026-01-14 22:30:49 +0100

    Add localisation to player information in getAllPlayerWithTheirLocalisation

--- PagesCodes/Sae_V2/app/models/lecteurDAO.py
+++ PagesCodes/Sae_V2/app/models/lecteurDAO.py
@@ -376,6 +376,7 @@ class lecteurDAO(lecteurDAOInterface):
 
                         return {
                             "ip":ip,
+                            "localisation":localisation,
                             "file":file,
                             "name":name,
                             "elapsed":elapsed,
